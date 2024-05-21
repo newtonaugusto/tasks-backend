06ad2aec1deccabb5cdca01848084f9ada249e79
RodrigoCelso <rodrigo.ca.bonfim@gmail.com>
2024-05-21 18:58:33 -0400

    chore: alterar arquivos de configuracao para trabalhar com Jenkins

--- src/main/java/br/ce/wcaquino/taskbackend/TaskBackendApplication.java
+++ src/main/java/br/ce/wcaquino/taskbackend/TaskBackendApplication.java
@@ -2,7 +2,6 @@ package br.ce.wcaquino.taskbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,10 +9,5 @@ public class TaskBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskBackendApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TaskBackendApplication.class);
 	}
 }
